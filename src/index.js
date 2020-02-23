4bf90a719f76e0ac329bf905fb65b80435ec71e3
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2020-02-23 22:38:03 +0600

    Added getters for users and tasks

--- src/index.js
+++ src/index.js
@@ -19,6 +19,27 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send("User not found")
+        }
+        res.status(200).send(user)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -26,6 +47,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send("Task not found")
+        }
+        res.status(200).send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
