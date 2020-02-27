9b5eca2b223a31f7af13f907265161c26897365e
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2020-02-27 22:12:40 +0600

    Cascading deleting task when user is removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -101,6 +102,14 @@ userSchema.pre('save', async function (next) {
     
     next()
 })
+
+// Delete user taks when user is removed
+userSchema.pre('remove', async function(next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id})
+    next()
+})
+
 
 const User = mongoose.model('User', userSchema)
 
