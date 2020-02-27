8e79fc2d661dd1c658e1bdb78e56c0c7bcd51e29
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2020-02-27 08:09:36 +0600

    refactoring REST API

---
+++ src/routers/task.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        return res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        return res.status(200).send(tasks)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+
+        if (!task) {
+            return res.status(404).send("Task not found")
+        }
+
+        return res.status(200).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every(update => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid operation'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, 
+            runValidators: true 
+        })
+
+        if (!task) {
+            return res.status(404).send("Task not found")
+        }
+
+        return res.status(200).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found'})
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
---
+++ src/routers/user.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../models/user')
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)    
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)        
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(update => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid operator' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, 
+            runValidators: true 
+        })
+
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send({ error: 'User not found'})
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
